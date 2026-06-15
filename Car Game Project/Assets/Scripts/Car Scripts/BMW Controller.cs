4d2ee0952a81dcbcddff5c26e8cce36e08e15bc5
Harjas2510 <singhharjasmeet6@gmail.com>
2026-06-15 11:26:04 -0700

    BMW

--- Car Game Project/Assets/Scripts/Car Scripts/BMW Controller.cs
+++ Car Game Project/Assets/Scripts/Car Scripts/BMW Controller.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BMWController : MonoBehaviour
 {
-
     public WheelCollider FrontRightWheelCollider;
     public WheelCollider RearRightWheelCollider;
     public WheelCollider FrontLeftWheelCollider;
@@ -15,26 +15,61 @@ public class BMWController : MonoBehaviour
     public Transform FrontLeftWheelTransform;
     public Transform FrontRightWheelTransform;
 
-    // Start is to begin
+    public float maxSteerAngle = 30f; 
+    // Added: How fast the wheels turn per second. Higher = faster, Lower = smoother/slower.
+    public float steeringSpeed = 150f; 
+
+    float VerticalInput;
+    float HorizontalInput;
+    float currentSteerAngle;
+    float targetSteerAngle; // Added: Tracks where the wheels *want* to go
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
     void FixedUpdate()
     {
         MotorForce();
+        SteerCar();    
         UpdateWheels();
     }
-    // void GetInput()
-    // {
-    //     GetInput.GetAxis();
-    // }
+
+    void GetInput()
+    {
+        if (Keyboard.current != null)
+        {
+            // Reads W/S or Up/Down Arrow keys
+            VerticalInput = 0f;
+            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) VerticalInput = 1f;
+            if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) VerticalInput = -1f;
+
+            // Reads A/D or Left/Right Arrow keys
+            HorizontalInput = 0f;
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) HorizontalInput = 1f;
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) HorizontalInput = -1f;
+        }
+    }
+
     void MotorForce()
     {
-        RearRightWheelCollider.motorTorque = 10f;
-        RearLeftWheelCollider.motorTorque = 10f;
+        RearRightWheelCollider.motorTorque = 2000f * VerticalInput;
+        RearLeftWheelCollider.motorTorque = 2000f * VerticalInput;
+    }
+
+    void SteerCar()
+    {
+        targetSteerAngle = maxSteerAngle * HorizontalInput;
+        currentSteerAngle = Mathf.MoveTowards(currentSteerAngle, targetSteerAngle, steeringSpeed * Time.deltaTime);
+    
+        FrontLeftWheelCollider.steerAngle = currentSteerAngle;
+        FrontRightWheelCollider.steerAngle = currentSteerAngle;
     }
 
     void UpdateWheels()
@@ -54,10 +89,4 @@ public class BMWController : MonoBehaviour
         WheelTransform.position = pos;
         WheelTransform.rotation = rot;
     }
-
-
-
-
-
-
 }
