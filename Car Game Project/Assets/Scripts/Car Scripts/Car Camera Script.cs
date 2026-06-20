02dbfd8018dd4ce1a70683785efbda359d5a1c59
Harjas2510 <singhharjasmeet6@gmail.com>
2026-06-19 17:22:37 -0700

    BMW

--- Car Game Project/Assets/Scripts/Car Scripts/Car Camera Script.cs
+++ Car Game Project/Assets/Scripts/Car Scripts/Car Camera Script.cs
@@ -6,13 +6,17 @@ public class CarCameraScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public Transform CarTransform;
-    public Transform CameraPointTransform;
-    public Transform ReverseCameraPointTransform;
+    public Transform MainCameraPointTransform;
+    public Transform ReverseCameraPointTransform; 
+    public Transform LeftCameraPointTransform;
+    public Transform RightCameraPointTransform;
     private Vector3 Velocity = Vector3.zero;
 
 
-
+    bool Left;
+    bool Right;
     bool Reverse;
+
     void Start()
     {
         
@@ -26,9 +30,18 @@ public class CarCameraScript : MonoBehaviour
     void LateUpdate()
     {
         transform.LookAt(CarTransform);
-        transform.position = Vector3.SmoothDamp(transform.position, CameraPointTransform.position, ref Velocity, 67f * Time.deltaTime); // To add delay in the camera movement
-
-
+        transform.position = Vector3.SmoothDamp(transform.position, MainCameraPointTransform.position, ref Velocity, 15f * Time.deltaTime); // To add delay in the camera movement
+        
+        if (Left == true) // To make camera change when reversing
+            {       
+                transform.LookAt(CarTransform);
+                transform.position = Vector3.SmoothDamp(transform.position, LeftCameraPointTransform.position, ref Velocity, 1f * Time.deltaTime); 
+            }
+        if (Right == true) // To make camera change when reversing
+            {       
+                transform.LookAt(CarTransform);
+                transform.position = Vector3.SmoothDamp(transform.position, ReverseCameraPointTransform.position, ref Velocity, 1f * Time.deltaTime); 
+            }
         if (Reverse == true) // To make camera change when reversing
         {       
             transform.LookAt(CarTransform);
